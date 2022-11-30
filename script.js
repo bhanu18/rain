f1c9b84209b204c1f0aff1f3f8adc64b2838d376
bhanuvidh <bhanuvidh@windowslive.com>
2022-11-30 22:51:12 +0700

    css fix

--- script.js
+++ script.js
@@ -29,11 +29,11 @@ var mini = true;
 
 function toggleSidebar() {
   if (mini) {
-    console.log("opening sidebar");
+   // console.log("opening sidebar");
     document.getElementById("mySidebar").style.width = "250px";
     this.mini = false;
   } else {
-    console.log("closing sidebar");
+   // console.log("closing sidebar");
     document.getElementById("mySidebar").style.width = "85px";
     this.mini = true;
   }
